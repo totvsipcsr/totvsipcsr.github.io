a74fbda5c34d7c0305d707b51352f323fa5d65af
Gabriel Da Silva Souza <gabriel.ssouza@totvs.com.br>
2021-08-23 07:29:26 -0300

    New Features

--- totvsipcsr.js
+++ totvsipcsr.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
     $(".ti").mouseenter(
         function() {
-            $(this).animate({ 'font-size': '140px' }, 500);
+            $(this).animate({ 'font-size': '123px' }, 500);
         });
     $(".ti").mouseleave(
         function() {
@@ -15,10 +15,15 @@ $(document).ready(function() {
 
         var height = $(window).height();
         var scroll_position = $(window).scrollTop();
-        var object_position_top = scroll_position + 393
+        var object_position_top = scroll_position + 450
 
         if (object_position_top >= height - 100) {
             object_position_top = height - 100;
+        }
+        if (object_position_top >= 500) {
+            $(".Initialblur").show(200);
+        } else {
+            $(".Initialblur").hide(200);
         }
         $('.initialPoint').css({
             'top': object_position_top
@@ -32,25 +37,60 @@ function ini() {
 
     $(".ti1").show(900);
     $(".titleInitial").animate({
-        "margin-left": "30%",
-    }, 1500, function() {
+        "margin-left": "22%",
+    }, 900, function() {
+        $(".ti2").show();
+        $(".ti2").animate({
+            "margin-left": "0px",
+        }, 200, function() {
+            $(".ti3").show();
+            $(".ti3").animate({
+                "margin-left": "0px",
+            }, 200, function() {
+                $(".ti4").show();
+                $(".ti4").animate({
+                    "margin-left": "0px",
+                }, 200, function() {
+                    $(".ti5").show();
+                    $(".ti5").animate({
+                        "margin-left": "0px",
+                    }, 200, function() {
+                        $(".ti6").show();
+                        $(".ti6").animate({
+                            "margin-left": "0px",
+                        }, 200, function() {
+                            $(".ti7").show();
+                            $(".ti7").animate({
+                                "margin-left": "0px",
+                            }, 200, function() {
+                                $(".ti8").show(800, function() {
+                                    $(".ti8").css("display", "block");
+                                    $(".waterMark").show(100, function() {
+                                        $(".waterMark2").show(150, function() {
+                                            interval = setInterval(showOptions, 150);
+                                        });
+                                    });
 
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });
     });
 
-    $(".ti2").show(1600);
-    $(".ti3").show(1700);
-    $(".ti4").show(1800);
-    $(".ti5").show(1900);
-    $(".ti6").show(2000);
-    $(".ti7").show(2100);
-    $(".ti8").show(3000);
 
-    interval = setInterval(showOptions, 3000);
 }
 
 var interval
 
 function showOptions() {
+    $(".ti8").animate({
+        "display": "block",
+    }, 200, function() {
+
+    });
     $(".bodyPage").show(500);
     $(".initialPoint").show(500);
     $(".socialButtons").show(500);
